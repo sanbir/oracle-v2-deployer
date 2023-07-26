1df412acce64a51ca54b8df2151da21bff844b5a
Alexander Biryukov <alexander.biryukov@p2p.org>
2023-07-26 14:36:15 +0300

    add server

--- deploy.ts
+++ deploy.ts
@@ -4,24 +4,21 @@ import {deployBatch} from "./scripts/deployBatch";
 import {DeployData} from "./scripts/models/DeployData";
 import {writeToCsv} from "./scripts/helpers/writeToCsv";
 
-async function main() {
-    const deployDataArray = await getDeployDataArray()
+export async function deploy() {
+    try {
+        const deployDataArray = await getDeployDataArray()
 
-    const batchSize = 50
-    const updatedDeployDataArray: DeployData[] = []
-    for (let i = 0; i < (deployDataArray.length / batchSize); i++) {
-        console.log('deploying batch #', i, '...')
-        const updatedDeployDataArray1 = await deployBatch(deployDataArray.slice(i * batchSize, (i + 1) * batchSize))
-        updatedDeployDataArray.push(...updatedDeployDataArray1)
-        console.log('batch #', i, 'deployed')
+        const batchSize = 50
+        const updatedDeployDataArray: DeployData[] = []
+        for (let i = 0; i < (deployDataArray.length / batchSize); i++) {
+            console.log('deploying batch #', i, '...')
+            const updatedDeployDataArray1 = await deployBatch(deployDataArray.slice(i * batchSize, (i + 1) * batchSize))
+            updatedDeployDataArray.push(...updatedDeployDataArray1)
+            console.log('batch #', i, 'deployed')
+        }
+
+        writeToCsv(updatedDeployDataArray)
+    } catch (error) {
+        console.error('Error on deploy', error);
     }
-
-    writeToCsv(updatedDeployDataArray)
 }
-
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
---
+++ index.ts
@@ -0,0 +1,78 @@
+import express, { Express, Request, Response } from 'express'
+import multer from 'multer'
+import fs from 'fs'
+import axios from 'axios'
+import FormData from 'form-data'
+import {deploy} from "./deploy"
+
+const upload = multer({ dest: 'uploads/' })
+
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Oracle V2 deploy server')
+})
+
+app.get('/recently-deployed-csv', async (req: Request, res: Response) => {
+    const outputCsvFilePath = process.env.OUTPUT_CSV_FILE_PATH
+    if (!outputCsvFilePath) {
+        res.status(500).send('No OUTPUT_CSV_FILE_PATH env')
+        return
+    }
+
+    // Check if the file exists before sending it
+    if (!fs.existsSync(outputCsvFilePath)) {
+        res.status(404).send('File not found.')
+        return
+    }
+
+    // Send the file to the client
+    res.sendFile(outputCsvFilePath)
+})
+
+app.post('/upload-csv', upload.single('file'), (req: Request, res: Response) => {
+    // Grab the URL from the request body
+    const { url } = req.body;
+
+    if (!req.file) {
+        res.status(400).send('No file in request')
+        return
+    }
+    if (!url) {
+        res.status(400).send('No url in request')
+        return
+    }
+
+    // Immediately return a HTTP 200 response
+    res.status(200).send('File upload received. Processing will begin shortly.')
+
+    // Move the uploaded file to the desired location
+    fs.rename(req.file.path, process.env.INPUT_CSV_FILE_PATH!, async (err: any) => {
+        if (err) {
+            console.error('Could not move uploaded file', err);
+            return;
+        }
+
+        await deploy()
+        await sendCsv(url)
+    });
+});
+
+async function sendCsv(url: string) {
+    try {
+        // Prepare the new CSV file for the POST request
+        const form = new FormData();
+        form.append('file', fs.createReadStream(process.env.OUTPUT_CSV_FILE_PATH!));
+
+        // Send a POST request to the provided URL with the parsed CSV data
+        const response = await axios.post(url, form, {
+            headers: form.getHeaders(),
+        });
+
+        console.log('Data sent successfully', response.data);
+    } catch (err) {
+        console.error('Error sending data', err);
+    }
+}
+
+app.listen(process.env.PORT, () => console.log('Server started on port', process.env.PORT));
