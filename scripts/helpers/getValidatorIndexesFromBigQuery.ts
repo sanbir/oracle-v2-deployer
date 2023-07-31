07b6424a930eab02115a53cec7a65fa14bbb3216
Alexander Biryukov <alexander.biryukov@p2p.org>
2023-07-31 15:04:25 +0300

    add KEY_FILE_NAME PROJECT_ID for BQ

--- scripts/helpers/getValidatorIndexesFromBigQuery.ts
+++ scripts/helpers/getValidatorIndexesFromBigQuery.ts
@@ -3,7 +3,10 @@ import {BigQuery} from "@google-cloud/bigquery";
 export async function getValidatorIndexesFromBigQuery(val_pubkeys: string[]) {
     console.log('Getting indexes from BigQuery....')
 
-    const bigquery = new BigQuery()
+    const bigquery = new BigQuery({
+        keyFilename: process.env.KEY_FILE_NAME,
+        projectId: process.env.PROJECT_ID
+    })
 
     const query = `
         SELECT val_id, val_pubkey FROM \`p2p-data-warehouse.raw_ethereum.validators_index\`
